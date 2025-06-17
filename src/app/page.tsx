6bcbc75c55588072faed7c4d8e344d873a0a28ec
rayysidd <siddiqui.rayyan@gmail.com>
2025-06-17 15:24:28 +0530

    deployed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home(){
     }
 
     try {
-      const response = await fetch('http://localhost:5001/api/chat/speech', {
+      const response = await fetch('https://mun-1igc.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
