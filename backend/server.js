55c93f4015065fe99243812b9b2a3d19c5ef0acb
rayysidd <siddiqui.rayyan@gmail.com>
2025-06-17 15:20:11 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
@@ -25,15 +25,6 @@ app.get('/',(req,res)=>{
 });
 // // Routes
 app.use('/api/chat', require('./routes/chat'));
-
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 
 const PORT = process.env.PORT || 5001;
